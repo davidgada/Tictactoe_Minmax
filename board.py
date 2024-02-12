ee3b59ce88f5409d7bf789d24f651e4f28c60160
David Agada <davidagada05@gmailcom>
2024-02-11 17:49:41 -0700

    pushing

--- board.py
+++ board.py
@@ -19,19 +19,71 @@ class Tictactoe:
             print('-'* 5)
 
     def checkWinner(self):
+        checker = False
         for i in range (3):
-            for j = i + 1 in range (3):
-                if self.board(j)
+            for j in range (3):
+                checker = self.board.comparePieces([i][j], [i][j + 1])
+                if j == 2:
+                    break
+        
+
+        for i in range (3):
+            for j in range (3):
+                checker = self.board.comparePieces([i][j], [i + 1][j])
+                if i == 2:
+                    break
+
+
+        i1 = 0
+        j1 = 0
+        while i1 < 3 and j1 < 3:
+            checker = self.board.comparePieces([i1][j1], [i1 + 1][j1 + 1])
+            i1 += 1
+            j1 += 1
+
+        i2 = 0
+        j2 = 2
+        while i2 < 3 and j2 < 3:
+            checker = self.board.comparePieces([i2][j2], [i2 + 1][j2 - 1])
+            i2+= 1
+            j2 += 1
+
+
+
+
+
+        for i in range (3):
+            for j in range (3):
+                checker = self.board.comparePieces([i][j], [i + 1],[j + 1])
+                if i == 2 or j == 2:
+                    break
+
+
+        for i in range (3):
+            for j in range (3):
+                checker = self.board.comparePieces([i][j], [i + 1],[j + 1])
+                if i == 2 or j == 2:
+                    break
+        
+        
+
+
+        
+        return checker
 
 
 
     def comparePieces(self, a:str, b:str):
-        if a == "A" or b == "A":
+        if a == "A":
+            return False
+        elif b == "A":
             return False
         elif a == "X" and b == "X":
             return True
-        elif a == "O" and b == "O"
+        elif a == "O" and b == "O":
             return True 
+        else:
+            return False
         
 
         

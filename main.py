17b2e47d060a994e758da8e74333b80cfde6c011
Ebubechukwu Agada <e.agada@uleth.ca>
2024-02-14 11:48:44 -0700

    check

--- main.py
+++ main.py
@@ -6,4 +6,15 @@ def main():
     board1 = Tictactoe()
     board1.printBoard()
 
+    board1.play(0,"X")
+    board1.play(1,"X")
+    board1.play(2,"X")
+    board1.printBoard()
+
+    if board1.checkWinner() == True:
+        print("Winning play")
+    else:
+        print("Losing stuff")
+
+
 main()
